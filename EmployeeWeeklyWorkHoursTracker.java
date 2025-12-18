39950cc43f1960ffa5dff6960bf5e47269f17158
GLORY FIE MENESES <g.meneses.545673@umindanao.edu.ph>
2025-12-18 14:18:19 +0800

    Refactor EmployeeWeeklyWorkHoursTracker class

--- EmployeeWeeklyWorkHoursTracker.java
+++ EmployeeWeeklyWorkHoursTracker.java
@@ -1,8 +1,6 @@
 package FinalsPractice;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class EmployeeWeeklyWorkHoursTracker {
 	
 	
@@ -118,3 +116,4 @@ public class EmployeeWeeklyWorkHoursTracker {
 	}
 
 }
+
