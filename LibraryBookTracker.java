11da613b618f2fc37c4fdab4c4643dd79aaecc44
GLORY FIE MENESES <g.meneses.545673@umindanao.edu.ph>
2025-12-19 21:40:48 +0800

    Refactor LibraryBookTracker for improved output
    DEBUG DEBUG

--- LibraryBookTracker.java
+++ LibraryBookTracker.java
@@ -1,127 +1,135 @@
-package MenesesLabActivities;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.util.Scanner;
-public class LibraryBookTracker {
+package FinalsPractice;
+import java.io.*;
+import java.util.*;
 
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.print("Enter number of books: ");
-	int num = sc.nextInt();
-	sc.nextLine();
-	
-	
-	String[] name = new String[num];
-	int[] qty = new int[num];
-	
-	
-	
-	
-	
-	for (int i = 0 ; i < num ; i++) {
+		public class LibraryBookTracker {
+		
+		public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number of books: ");
+		int num = sc.nextInt();
+		sc.nextLine();
+		
+		
+		String[] name = new String[num];
+		int[] qty = new int[num];
+		
+		
+		for (int i = 0 ; i < num ; i++) {
 		System.out.print("Enter title of book " + (i+1) + ": ");
 		name[i] = sc.nextLine();
 		
 		System.out.print("Enter copies of " + " " + name[i] + ": " );
 		qty[i] = sc.nextInt();
 		sc.nextLine();
-	}
-	
-	
-	
-	
-	
-	int total = totalbooks(qty);
-	int instock = booksinstock(qty);
-	int outstock = booksoutstock(qty);
-	
-	
-	
-	
-	System.out.print("To view summary, refer to text file Library Report");
-	
-	
-	try {
+		
+		}
+		
+		
+		
+		// do methods first and then declare the following
+		
+		int total = totalbooks(qty);
+		int instock = booksinstock(qty);
+		int outstock = booksoutstock(qty);
+		
+		System.out.println();
+		
+		System.out.print("Books and Copies: \n");
+		System.out.println();
+		for (int i = 0; i < name.length; i++) {
+			System.out.print(name[i] + " - " + qty[i] + "\n");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Total Copies: " + total);
+		System.out.println("Available: " + instock);
+		System.out.println("Unavailable: " + outstock);
+		
+		System.out.println();
+		
+		System.out.println("To view summary, refer to text file Library Report");
+		
+		try {
 		
 		FileWriter fw = new FileWriter("LibraryReport.txt");
 		PrintWriter pw = new PrintWriter(fw);
 		
 		pw.println("Books and Copies: ");
-		for (int i = 0 ; i < num ; i++) {
-			pw.println(name[i] + " - " + qty[i]);
-		}
 		
+		for (int i = 0 ; i < num ; i++) {
+		
+		pw.println(name[i] + " - " + qty[i]);
+		
+		}
 		
 		pw.println("Total Copies: " + total);
 		pw.println("Available: " + instock);
 		pw.println("Unavailable: " + outstock);
-		
-		
-		
-		
 		fw.close();
 		pw.close();
 		
-	}
-	
-	catch (IOException e) {
+		}
+		
+		catch (IOException e) {
+		
 		System.out.print("Error writing file to text.");
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	sc.close();
-
-	} // closing bracket of the main method
-	
-	
-	static int totalbooks(int[] qty) {
-		int totalcounter = 0;
-		for (int q : qty) {
-			totalcounter += q;
+		
 		}
+		
+		sc.close();
+		
+		} // closing bracket of the main method
+		
+		
+		
+		
+		
+		// computing total books
+		static int totalbooks(int[] qty) {
+			int totalcounter = 0;
+		
+			for (int q : qty) {
+				totalcounter += q;
+		
+		}
+		
 		return totalcounter;
-	}
-	
-	
-	static int booksinstock(int[] qty) {
-		int instock = 0;
-		for (int q : qty) {
+		
+		}
+		
+		
+		// computing books in stock
+		
+		static int booksinstock(int[] qty) {
+			int instock = 0;
+			for (int q : qty) {
+			
 			if (q > 0) {
-				instock++;
+			
+			instock++;
+			
 			}
+			
 		}
-		return instock;
-	}
-	
-	
-	static int booksoutstock(int[] qty) {
-		int outstock = 0;
-		for (int q : qty) {
-			if (q == 0) {
-				outstock++;
-			}
-		}
-		return outstock;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-}
+		return instock;
+			
+		}
+		
+		static int booksoutstock(int[] qty) {
+			int outstock = 0;
+			for (int q : qty) {
+			if (q == 0) {
+			outstock++;
+		}
+			
+			}
+			
+			return outstock;
+			
+			}
+		
+		}
